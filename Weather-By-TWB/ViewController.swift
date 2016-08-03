e232075e51b1560a7c0ae1232050abda128677e5
Thomas Bishop <thomasbishop@yahoo.com>
2016-08-03 19:33:03 -0400

    Initial Commit

---
+++ Weather-By-TWB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather-By-TWB
+//
+//  Created by Thomas Bishop on 8/3/16.
+//  Copyright © 2016 Bishop Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
